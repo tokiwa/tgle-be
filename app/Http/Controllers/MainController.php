bc20194939fb272658bf42b7bbf776fdc720252f
Yuji Tokiwa <tokiwa@yujitokiwa.jp>
2023-01-13 22:05:52 +0900

    tgle3-38 implemented

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -23,7 +23,8 @@ class MainController extends Controller
 
     public function mkgroup(Request $request)
     {
-        function postJson($url, $data){
+        function postJson($url, $data)
+        {
             $ch = curl_init();
 
             curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
@@ -31,9 +32,9 @@ class MainController extends Controller
             curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($ch, CURLOPT_URL, $url);
-            $result=curl_exec($ch);
+            $result = curl_exec($ch);
             return $result;
-         //   echo 'RETURN:'.$result;
+            //   echo 'RETURN:'.$result;
         }
 
         $data = $request->input();
@@ -42,13 +43,13 @@ class MainController extends Controller
         $kvdata = array();
         $studentid = array();
 
-        $users = Keyword::where('lessonid',$lessonid)->where('role',"learner")->groupBy('userid')->get(['userid']);
-        foreach ($users as $user){
+        $users = Keyword::where('lessonid', $lessonid)->where('role', "learner")->groupBy('userid')->get(['userid']);
+        foreach ($users as $user) {
             $userid = $user->userid;
-            $latest = Keyword::where('lessonid',$lessonid)->where('userid',$userid)->latest()->first();
-            $lastkeywords = Keyword::where('lessonid',$lessonid)->where('userid',$userid)->where('sessionid',$latest->sessionid)->get();
+            $latest = Keyword::where('lessonid', $lessonid)->where('userid', $userid)->latest()->first();
+            $lastkeywords = Keyword::where('lessonid', $lessonid)->where('userid', $userid)->where('sessionid', $latest->sessionid)->get();
             $keyword = array();
-            foreach ($lastkeywords as $lastkeyword){
+            foreach ($lastkeywords as $lastkeyword) {
                 $keyword[] = $lastkeyword->keyword;
             }
             array_push($studentid, $userid);
@@ -57,13 +58,13 @@ class MainController extends Controller
 
         # Get Instructor Keyword = Group Keyword
         $groupkw = array();
-        $users = Keyword::where('lessonid',$lessonid)->where('role',"instructor")->groupBy('userid')->get(['userid']);
-        foreach ($users as $user){
+        $users = Keyword::where('lessonid', $lessonid)->where('role', "instructor")->groupBy('userid')->get(['userid']);
+        foreach ($users as $user) {
             $userid = $user->userid;
-            $latest = Keyword::where('lessonid',$lessonid)->where('userid',$userid)->latest()->first();
-            $lastkeywords = Keyword::where('lessonid',$lessonid)->where('userid',$userid)->where('sessionid',$latest->sessionid)->get();
+            $latest = Keyword::where('lessonid', $lessonid)->where('userid', $userid)->latest()->first();
+            $lastkeywords = Keyword::where('lessonid', $lessonid)->where('userid', $userid)->where('sessionid', $latest->sessionid)->get();
             $keyword = array();
-            foreach ($lastkeywords as $lastkeyword){
+            foreach ($lastkeywords as $lastkeyword) {
                 $keyword[] = $lastkeyword->keyword;
             }
             array_push($groupkw, $keyword);
@@ -71,26 +72,26 @@ class MainController extends Controller
         # userid: Instructor ID
         $groupKeyword = $groupkw[0];
 
-        $jdata =  array('student' => $kvdata, 'groupKeyword' => $groupKeyword);
+        $jdata = array('student' => $kvdata, 'groupKeyword' => $groupKeyword);
         $data_json = json_encode($jdata, JSON_UNESCAPED_UNICODE);
         $url = 'http://192.168.1.105:9700/mkgroup';
         $res = postJson($url, $data_json);  //mac miniに実装したw2vを用いたグループ生成ツールを呼び出す
 
-        $arr = json_decode($res,true);
-        $arr1 = json_decode($arr,true);
+        $arr = json_decode($res, true);
+        $arr1 = json_decode($arr, true);
 
 //        dd($studentid);  //dump & quit()
 
 //  グループ化された結果をgroupsに書き込む
-        foreach( $arr1 as $key => $value ){
-            $i =  (int) $key;
-            $j = (int) $value;
+        foreach ($arr1 as $key => $value) {
+            $i = (int)$key;
+            $j = (int)$value;
             $group = new Group;
             $group->userid = $studentid[$i];
             $group->lessonid = $lessonid;
 //            $group->groupid = (string) $value;
             $group->groupid = $groupKeyword[$j];
-//            $group->save();
+            $group->save();
         }
 
 //        return response()->json(['result' => 'Success']);
@@ -144,7 +145,7 @@ class MainController extends Controller
 
 //        $label = 'u3003';
 //        $lessons = Lesson::where('academicyear',2021)->pluck('lessontitle','id');
-        $lessons = Lesson::where('academicyear',$academicyear)->where('label',$label)->where('status',$status)->get();
+        $lessons = Lesson::where('academicyear', $academicyear)->where('label', $label)->where('status', $status)->get();
 
         return response()->json($lessons);
     }
@@ -157,16 +158,16 @@ class MainController extends Controller
         $user_keyword = array();
 
 //        $users = Keyword::where('lessonid',$lessonid)->groupBy('userid')->get(['userid']);
-        $users = Keyword::where('lessonid',$lessonid)->where('role','learner')->groupBy('userid')->get(['userid']);
-        foreach ($users as $user){
+        $users = Keyword::where('lessonid', $lessonid)->where('role', 'learner')->groupBy('userid')->get(['userid']);
+        foreach ($users as $user) {
             $userid = $user->userid;
-            $latest = Keyword::where('lessonid',$lessonid)->where('userid',$userid)->latest()->first();
-            $lastkeywords = Keyword::where('lessonid',$lessonid)->where('userid',$userid)->where('sessionid',$latest->sessionid)->get();
+            $latest = Keyword::where('lessonid', $lessonid)->where('userid', $userid)->latest()->first();
+            $lastkeywords = Keyword::where('lessonid', $lessonid)->where('userid', $userid)->where('sessionid', $latest->sessionid)->get();
             $keyword = array();
-            foreach ($lastkeywords as $lastkeyword){
+            foreach ($lastkeywords as $lastkeyword) {
                 $keyword[] = $lastkeyword->keyword;
             }
-        $user_keyword[] = array("user" => $userid,"keyword" => $keyword);
+            $user_keyword[] = array("user" => $userid, "keyword" => $keyword);
         }
 
         return response()->json($user_keyword);
@@ -177,16 +178,31 @@ class MainController extends Controller
         $data = $request->input();
         $lessonid = $data['lessonid'];
         $role = $data['role'];
+        $user_id = $data['user_id'];
 
         $user_group = array();
+        $group_member = array();
 
-        $users = Group::where('lessonid',$lessonid)->groupBy('userid')->get(['userid']);
-        foreach ($users as $user){
-            $userid = $user->userid;
-            $latest = Group::where('lessonid',$lessonid)->where('userid',$userid)->latest()->first();
-            $user_group[] = array("group" => $latest->groupid,"user" => $userid);
+        if ($role == 'instructor') {
+            $users = Group::where('lessonid', $lessonid)->groupBy('userid')->get(['userid']);
+            foreach ($users as $user) {
+                $userid = $user->userid;
+                $latest = Group::where('lessonid', $lessonid)->where('userid', $userid)->latest()->first();
+                $user_group[] = array("group" => $latest->groupid, "user" => $userid);
+            }
+            return response()->json($user_group);
         }
-        return response()->json($user_group);
+
+        if ($role == 'learner') {
+            $user = Group::where('lessonid', $lessonid)->where('userid', $user_id)->latest()->first();
+            $users = Group::where('lessonid', $lessonid)->where('groupid', $user->groupid)->groupBy('userid')->get(['userid']);
+            foreach ($users as $user) {
+                $userid = $user->userid;
+                $latest = Group::where('lessonid', $lessonid)->where('userid', $userid)->latest()->first();
+                $group_member[] = array("group" => $latest->groupid, "user" => $userid);
+            }
+            return response()->json($group_member);
+        }
     }
 
 }
